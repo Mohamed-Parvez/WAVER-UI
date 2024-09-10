32da1bb5391bf3368537c769ce35da566528ada4
Mohamed-Parvez <clash.parwesh@gmail.com>
2024-09-10 21:50:32 +0530

    updated footer

--- app/app/(root)/(home)/page.tsx
+++ app/app/(root)/(home)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <section className="container-2xl mt-8">
       <div className="flex-center flex-col space-y-5 sm:space-y-6 lg:space-y-8">
         <Link
-          href={"/components"}
+          href={"/appui"}
           className="px-4 py-1.5 border hover:border-slate-300 dark:hover:border-slate-500 cursor-pointer border-slate-100 dark:border-gray-800 rounded-full shadow-sm bg-slate-100 dark:bg-gray-800 flex-center gap-2"
         >
           <p className="extra-small-regular xs:small-regular text-gray-900 dark:text-gray-200">
@@ -29,13 +29,13 @@ export default function Home() {
         </div>
         <div className="flex-col space-y-3.5 items-center justify-center max-w-[400px] w-full xs:w-auto xs:space-y-0 xs:flex-center xs:flex-row gap-4 md:gap-5">
           <Link
-            href={"/components"}
+            href={"/appui"}
             className="px-4 flex-center py-[8px] lg:py-[12px] dark:bg-white dark:text-black bg-gray-800 text-white max-w-[400px] w-full xs:w-auto rounded-[8px] body-regular"
           >
             Get Started
           </Link>
           <Link
-            href={"/components"}
+            href={"/appui"}
             className="px-4 py-[8px] lg:py-[12px] border border-slate-300 text-gray-900 rounded-[8px] body-regular w-full max-w-[400px] xs:w-auto dark:text-white flex-center gap-2"
           >
             <Star size={20} />
--- app/constants/index.ts
+++ app/constants/index.ts
@@ -11,3 +11,37 @@ export const AppUiTag = [
   { key: 2, label: "Carousel", href: "/appui/carousel" },
   { key: 3, label: "Input", href: "/appui/input" },
 ];
+
+export const Footer_AppUi_Items = [
+  { key: 1, label: "Avatar", href: "/appui" },
+  { key: 2, label: "Carousel", href: "/appui/carousel" },
+  { key: 3, label: "Input", href: "/appui/input" },
+  { key: 4, label: "Avatar", href: "/appui" },
+];
+
+export const Footer_Section_Items = [
+  { key: 1, label: "Avatar", href: "/appui" },
+  { key: 2, label: "Carousel", href: "/appui/carousel" },
+  { key: 3, label: "Input", href: "/appui/input" },
+  { key: 4, label: "Avatar", href: "/appui" },
+];
+
+export const Footer_Icon_Items = [
+  { key: 1, label: "Avatar", href: "/appui" },
+  { key: 2, label: "Carousel", href: "/appui/carousel" },
+  { key: 3, label: "Input", href: "/appui/input" },
+  { key: 4, label: "Avatar", href: "/appui" },
+];
+export const Footer_Color_Items = [
+  { key: 1, label: "Avatar", href: "/appui" },
+  { key: 2, label: "Carousel", href: "/appui/carousel" },
+  { key: 3, label: "Input", href: "/appui/input" },
+  { key: 4, label: "Avatar", href: "/appui" },
+];
+
+export const FooterImageItems = [
+  { key: 1, label: "x", href: "/", src: "/assets/icons/x.svg" },
+  { key: 2, label: "Instagram", href: "/", src: "/assets/icons/instagram.svg" },
+  { key: 1, label: "Linkedin", href: "/", src: "/assets/icons/linkedin.svg" },
+  { key: 1, label: "Youtube", href: "/", src: "/assets/icons/youtube.svg" },
+];
